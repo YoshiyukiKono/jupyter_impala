2b8de9ece5f9ef96497ef96363063475c2ae679e
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-02-25 13:34:08 +0900

    Create git_env.sh

---
+++ git_env.sh
@@ -0,0 +1,6 @@
+git config --global user.name YoshiyukiKono
+git config --global user.email yoshiyuki.kono@gmail.com
+export GIT_AUTHOR_NAME=YoshiyukiKono
+export GIT_COMMITTER_NAME=YoshiyukiKono
+export GIT_COMMITTER_EMAIL=yoshiyuki.kono@gmail.com
+export GIT_AUTHOR_EMAIL=yoshiyuki.kono@gmail.com
